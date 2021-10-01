60e14cd9fe174dfb6494256d0e4fd129196a7729
Stephen Affanyi <swatiqman@gmail.com>
2021-10-01 16:04:59 +0000

    app environment interface

--- libs/interfaces/src/lib/system.interface.ts
+++ libs/interfaces/src/lib/system.interface.ts
@@ -1,7 +1,33 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 export namespace SystemInterfaces {
+
+  export type IdentifierType = 'email' | 'tel' | 'text'
+  export interface Identifier {
+    type: IdentifierType
+    label: string
+    loginSubmitLabel: string;
+    registerSubmitLabel: string;
+    otpType?: string
+    requiresOtp: boolean;
+  }
   export interface SessionInterface {
     projectId: string;
     tenantId: string;
+  }
+
+  export interface ApiResponse<T> {
+    statusCode: number;
+    data: T;
+  }
+  export interface Environment {
+    production: boolean;
+    baseUrl: {[id: string]: string};
+    client_id: string;
+    client_secret: string;
+    identifier: Identifier;
+    loginUrl: string;
+    registerUrl: string;
+    redirectUrl: string;
+    homeUrl: string;
   }
 }
