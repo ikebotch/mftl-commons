5739cd55e01b9ed70d0eb2d338eb6c2d9829fb65
Stephen Affanyi <swatiqman@gmail.com>
2021-10-01 16:05:44 +0000

    authenticationPost and responce Interface

--- libs/interfaces/src/lib/iam.interfaces.ts
+++ libs/interfaces/src/lib/iam.interfaces.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 export namespace IamInterfaces {
+
+  export type GrantType = 'client-credentials' | 'password' | 'refresh-token'
   export interface AuthenticationToken {
     uid: string;
     iss: string;
@@ -9,4 +11,17 @@ export namespace IamInterfaces {
     iat: number;
     exp: number;
   }
+
+  export interface AuthenticationCredential {
+      identifier: string;
+      password: string;
+      grant_type: GrantType;
+  }
+
+  export interface AuthenticationResponseData {
+    token: string;
+    refreshToken: string;
+    createdAt: Date;
+    expiry: Date;
+}
 }
