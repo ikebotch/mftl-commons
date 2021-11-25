7bac5ec127558a4b7b138dd749b7218c74ff2260
Reginald Addo <addoreginald360@gmail.com>
2021-11-25 09:19:24 +0000

    added project interceptor

--- libs/tenancy/src/lib/tenancy.service.ts
+++ libs/tenancy/src/lib/tenancy.service.ts
@@ -3,6 +3,7 @@ import { Injectable, Scope } from '@nestjs/common';
 @Injectable({ scope: Scope.REQUEST })
 export class TenancyService {
   private tenantId = '';
+  private projectId = '';
 
   public get tenancy(): string {
     return this.tenantId;
@@ -10,5 +11,13 @@ export class TenancyService {
 
   public set tenancy(tenantId: string) {
     this.tenantId = tenantId;
+  }
+
+  public get project(): string {
+    return this.projectId;
+  }
+
+  public set project(projectId: string) {
+    this.projectId = projectId;
   }
 }
