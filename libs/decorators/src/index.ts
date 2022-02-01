f5ca5ca1346dd62f53cadef7cab034c87514742c
Reginald Addo <addoreginald360@gmail.com>
2022-02-01 11:17:41 +0000

    added project id to decorators

--- libs/decorators/src/index.ts
+++ libs/decorators/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/role.decorators';
 export * from './lib/tenant-id.decorator';
 export * from './lib/user.decorator';
 export * from './lib/auth-token.decorator';
+export * from './lib/project-id.decorators';
